51dbf5eeb8d490bac1aee5c1b36d2a09da4c9718
SchoolnikVlad <allbasa1337@gmail.com>
2024-06-24 14:20:24 +0300

    правки дз 9

--- lesson_9/test_database.py
+++ lesson_9/test_database.py
@@ -1,7 +1,7 @@
 from sqlalchemy import create_engine
 from sqlalchemy import text
 
-db_connection_string = "postgres://x_clients_db_3fmx_user:mzoTw2Vp4Ox4NQH0XKN3KumdyAYE31uq@dpg-cour99g21fec73bsgvug-a.oregon-postgres.render.com/x_clients_db_3fmx"
+db_connection_string = "postgresql://x_clients_db_3fmx_user:mzoTw2Vp4Ox4NQH0XKN3KumdyAYE31uq@dpg-cour99g21fec73bsgvug-a.oregon-postgres.render.com/x_clients_db_3fmx"
 
 
 def test_db_connection():
